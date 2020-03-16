3f3b9a04a6643c27aeca3ad12028db67c8f6ff98
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2020-03-16 19:11:03 +0000

    Corrected error when error is larger than value

--- analyse_data/scripts/clean_value_error.py
+++ analyse_data/scripts/clean_value_error.py
@@ -4,15 +4,17 @@ import numpy as np
 # Get index of the last zero in the error
 def last_zero( string_error ):
     """ 
-    Returns the last zero encountered in a error string. This error corresponds to 
-    the rounding error.
+    Returns the last zero encountered in a error string. This error 
+    corresponds to the rounding error.
     Arguments:
         string_error (string):
             String containing the value of the error to be rounded
     Returns:
         index_last (int):
-            Position of the last zero from the beginning in string_error
+            Position of the last zero from the beginning in 
+            string_error
     """
+    index_last = 0
     for index, char in enumerate( string_error ):
         if char == '0' or char == '.':
             index_last = index
@@ -29,6 +31,7 @@ error = sys.argv[2]
 pos_zero = last_zero( error )
 clean_error = np.round( float( error ), pos_zero )
 clean_value = np.round( float( value ), pos_zero )
+
 
 # Return the value 
 print( clean_value, clean_error )
